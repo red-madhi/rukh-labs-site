332f5013e839b49fd1e92e455ed42dd98ed4eec7
Red Madhi <red.madhi@gmail.com>
2026-08-23 09:00:20 -0600

    Allow containers to shrink on mobile

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 export function Container({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("mx-auto w-full max-w-7xl px-5 sm:px-6 lg:px-8", className)}
+      className={cn("mx-auto w-full min-w-0 max-w-7xl px-5 sm:px-6 lg:px-8", className)}
       {...props}
     />
   );
